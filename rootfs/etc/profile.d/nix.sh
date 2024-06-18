283186f8dc25cb5dd5e8fdac3f46e793ba2efb6a
Agus Lopez <contact@aguslr.com>
2024-06-18 14:48:37 +0200

    Append `defexpr/channels` to NIX_PATH
    https://discourse.nixos.org/t/where-is-nix-path-supposed-to-be-set/16434/8

--- rootfs/etc/profile.d/nix.sh
+++ rootfs/etc/profile.d/nix.sh
@@ -10,6 +10,12 @@ if systemctl --quiet is-active nix.mount; then
 		mkdir -p "${NIX_STATE_HOME}"
 		echo 'https://nixos.org/channels/nixpkgs-unstable nixpkgs' > "${NIX_STATE_HOME}/channels"
 	fi
+	# Append `defexpr/channels` to NIX_PATH
+	if [ -r "${NIX_STATE_HOME}/defexpr/channels" ]; then
+		export NIX_PATH=${NIX_PATH:+$NIX_PATH:}${NIX_STATE_HOME}/defexpr/channels
+	elif [ -r "${HOME}/.nix-defexpr/channels" ]; then
+		export NIX_PATH=${NIX_PATH:+$NIX_PATH:}$HOME/.nix-defexpr/channels
+	fi
 	unset NIX_STATE_HOME
 
 fi
